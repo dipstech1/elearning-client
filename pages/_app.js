058da918ebc38bd6b94c1af6c50411299d2a5bc8
dipstech1 <panja.souradeep@gmail.com>
2022-03-19 11:24:24 +0530

    Initial UI setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/antd.css'
+import "../public/css/styles.css"
+
+import TopNav from '../components/TopNav'; 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <TopNav/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ pages/login.js
@@ -0,0 +1,11 @@
+
+const login = () => {
+  return (
+    <>
+      <h1 className='navHeader text-center p-5 bg-primary text-white  square'>Login</h1>
+      <p>Login..</p>
+    </>
+  )
+}
+
+export default login
